c7c2046d688fea0bd5bb9490857c6cb7ee991b3e
Stuart Wheater <stuart.wheater@arjuna.com>
2014-10-04 23:25:37 +0100

    Set bind address

--- databroker_webportal/add_files/run.sh
+++ databroker_webportal/add_files/run.sh
@@ -4,4 +4,8 @@ su - postgres --command="pg_ctl start -l /tmp/serverlog"
 
 sleep 5
 
-su - wildfly --command="/opt/wildfly/bin/standalone.sh -b=0.0.0.0 -bmanagement=0.0.0.0"
+if [ -n ${BIND_ADDRESS} ]; then
+    su - wildfly --command="/opt/wildfly/bin/standalone.sh -b=${BIND_ADDRESS} -bmanagement=${BIND_ADDRESS}"
+else
+    su - wildfly --command="/opt/wildfly/bin/standalone.sh -b=0.0.0.0 -bmanagement=0.0.0.0"
+fi
