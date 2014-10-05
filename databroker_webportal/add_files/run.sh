2015045646b72e7a4bb3f3147c7e47ca3a4401e2
Stuart Wheater <stuart.wheater@arjuna.com>
2014-10-05 22:38:01 +0100

    Partial fix

--- databroker_webportal/add_files/run.sh
+++ databroker_webportal/add_files/run.sh
@@ -4,7 +4,7 @@ su - postgres --command="pg_ctl start -l /tmp/serverlog"
 
 sleep 5
 
-if [ -n ${BIND_ADDRESS} ]; then
+if [ -n "${BIND_ADDRESS}" ]; then
     su - wildfly --command="/opt/wildfly/bin/standalone.sh -b=${BIND_ADDRESS} -bmanagement=${BIND_ADDRESS}"
 else
     su - wildfly --command="/opt/wildfly/bin/standalone.sh -b=0.0.0.0 -bmanagement=0.0.0.0"
